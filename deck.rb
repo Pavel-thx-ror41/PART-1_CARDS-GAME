29b63877d64f2cac89a0f4106429c179958d2a75
User Name <user@host>
2023-06-11 13:21:43 +0300

    WIP deck codestyle

--- deck.rb
+++ deck.rb
@@ -2,26 +2,17 @@
 
 # Колода карт
 class Deck
-  ONE =    '🂢🂲🃂🃒🂣🂳🃃🃓🂤🂴🃄🃔🂥🂵🃅🃕🂦🂶🃆🃖🂧🂷🃇🃗🂨🂸🃈🃘🂩🂹🃉🃙🂪🂺🃊🃚'.chars
-  # .each_slice(4).to_a
-  TEN =    '🂫🂻🃋🃛🂭🂽🃍🃝🂮🂾🃎🃞'.chars
-  ELEVEN = '🂡🂱🃁🃑'.chars
+  DIGITS = '🂢🂲🃂🃒🂣🂳🃃🃓🂤🂴🃄🃔🂥🂵🃅🃕🂦🂶🃆🃖🂧🂷🃇🃗🂨🂸🃈🃘🂩🂹🃉🃙🂪🂺🃊🃚'.chars
+  FIGURES = '🂫🂻🃋🃛🂭🂽🃍🃝🂮🂾🃎🃞'.chars
+  ACES = '🂡🂱🃁🃑'.chars
   CARD_BACK = '🂠'
 
   def self.card_back
     CARD_BACK
   end
 
-  def self.score_for_card_with_score(card, curr_score)
-    [
-      *Deck::ONE.zip(Array.new(Deck::ONE.size, 1)),
-      *Deck::TEN.zip(Array.new(Deck::TEN.size, 10)),
-      *Deck::ELEVEN.zip(Array.new(Deck::ELEVEN.size, (curr_score + 11) > 21 ? 1 : 11))
-    ].to_h[card]
-  end
-
   def initialize
-    @cards = [*ONE, *TEN, *ELEVEN].shuffle!(random: Random.new((Time.now.to_f * 1000).to_i))
+    @cards = [*DIGITS, *FIGURES, *ACES].shuffle!(random: Random.new((Time.now.to_f * 1000).to_i))
   end
 
   def card_get
