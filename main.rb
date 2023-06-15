421f6422e6a195a475b22e080134604028ff52f3
User Name <user@host>
2023-06-15 18:45:55 +0300

    extract tests

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ require_relative 'deck'
 require_relative 'dealer'
 require_relative 'player'
 require_relative 'game'
+require_relative 'tests'
 
 # UI
 class UI
@@ -32,7 +33,7 @@ class App
   INITIAL_PLAYERS_MONEY = 100
 
   def run
-    test
+    Tests.new.test
 
     dealer = Dealer.new(INITIAL_PLAYERS_MONEY)
     player = Player.new(UI.ask('Введите Ваше имя'), INITIAL_PLAYERS_MONEY)
@@ -58,43 +59,6 @@ class App
       puts
       exit if player.money < BET || dealer.money < BET || !UI.ask('Играем ещё? (Y/N)').to_s.upcase.eql?('Y')
     end
-  end
-
-  private
-
-  def test
-    test_deck
-    test_game
-  end
-
-  def test_deck
-    deck = Deck.new
-    1..52.times { deck.card_get }
-
-    begin
-      wrong_card = deck.card_get
-    rescue StandardError
-      # do nothing
-    end
-    raise 'Проверка Колоды. В колоде должно быть 52 карты' if wrong_card
-  end
-
-  def test_game
-
-    # Подсчёт очков
-    # Цифры
-    raise 'Проверка Игры. Проверка посчёта очков для 🂧.' unless
-      Game.score_for_card_with_score('🂧', 10) == 7
-
-    raise 'Проверка Игры. Проверка посчёта очков для 🃃.' unless
-      Game.score_for_card_with_score('🃃', 21) == 3
-
-    # Туз
-    raise 'Проверка Игры. Проверка посчёта очков для 🃑.' unless
-      Game.score_for_card_with_score('🃑', 10) == 11
-
-    raise 'Проверка Игры. Проверка посчёта очков для 🂱.' unless
-      Game.score_for_card_with_score('🂱', 11) == 1
   end
 end
 
---
+++ tests.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: false
+
+# Тесты
+class Tests
+  def test
+    test_deck
+    test_game
+  end
+
+  def test_deck
+    deck = Deck.new
+    1..52.times { deck.card_get }
+
+    begin
+      wrong_card = deck.card_get
+    rescue StandardError
+      # do nothing
+    end
+    raise 'Проверка Колоды. В колоде должно быть 52 карты' if wrong_card
+  end
+
+  def test_game
+
+    # Подсчёт очков
+    # Цифры
+    raise 'Проверка Игры. Проверка посчёта очков для 🂧.' unless
+      Game.score_for_card_with_score('🂧', 10) == 7
+
+    raise 'Проверка Игры. Проверка посчёта очков для 🃃.' unless
+      Game.score_for_card_with_score('🃃', 21) == 3
+
+    # Туз
+    raise 'Проверка Игры. Проверка посчёта очков для 🃑.' unless
+      Game.score_for_card_with_score('🃑', 10) == 11
+
+    raise 'Проверка Игры. Проверка посчёта очков для 🂱.' unless
+      Game.score_for_card_with_score('🂱', 11) == 1
+  end
+end
